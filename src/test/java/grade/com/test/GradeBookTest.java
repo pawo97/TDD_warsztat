62883ce3ff631b1e8fa50b8e2a01fad44cc4cae7
p.wolski@polsource.com <pa-wo97@o2.pl>
2020-11-29 17:47:01 +0100

    Add Red Test 1

--- src/test/java/grade/com/test/GradeBookTest.java
+++ src/test/java/grade/com/test/GradeBookTest.java
@@ -10,17 +10,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GradeBookTest {
 
     //Your code here.....
+    private static final String SUBJECT = "matematyka";
+    private static final String OTHER_SUBJECT = "fizyka";
 
     //Your code here.....
+    private GradeBook gradeBookWithSubject;
 
     @BeforeEach
     void setUp() {
         //Your code here.....
+        gradeBookWithSubject = GradeBook.getInstance();
+        gradeBookWithSubject.addSubject(SUBJECT);
     }
 
     @Test
     void shouldBeAbleToCreateGradeBook() {
         //Your code here.....
+        GradeBook gradeBook = GradeBook.getInstance();
+        assertNotNull(gradeBook);
     }
 
 //    @Test
